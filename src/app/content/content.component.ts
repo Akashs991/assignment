d10d706cce8a0fcc9e34c5e11179c2c11869e8c5
Akash Shrivastav <akash.shrivastav@ing.com>
2022-06-02 13:20:55 +0530

    feat: inital commit

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css'],
+})
+export class ContentComponent {
+  customerDetails = [
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+    {
+      companyName: 'Greenstone Financial',
+      name: 'John Snow',
+      address: '123 Main St, Philadelphia, PA 19147',
+      email: 'jsnow@company.com',
+      logo: 'assets/think-icon.png',
+    },
+  ];
+  constructor() {}
+}
